0963f214dd79e65368e52362dcaf192af0a8fdb4
Sebastián Vallejo <sebasvallejo@gmail.com>
2025-10-10 13:00:24 -0500

    Update

--- src/UI/download.CV.button.tsx
+++ src/UI/download.CV.button.tsx
@@ -2,7 +2,7 @@ import{ FaFileDownload } from "react-icons/fa";
 
 const DownloadButton = () => {
 	return (
-		<a href="/assets/images/CV.portfolio" download>
+		<a href="/assets/CV.portfolio.pdf" download>
 			<button className="text-black bg-white font-subtitle min-w-[10px] flex items-center gap-2 hover:bg-black hover:text-white hover:border-white transition-all duration-300">
 				<FaFileDownload /> Download CV
 			</button>
